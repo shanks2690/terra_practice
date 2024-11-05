600b8f87544e6ccf310c8ef1e8db7096feedb979
shanks2690 <Shanks.git@2826>
2024-11-05 20:04:22 +0530

    My terraform practice project

--- terraform code/main.tf
+++ terraform code/main.tf
@@ -1,10 +1,18 @@
 
+
 terraform {
   required_version = ">=1.9.0"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = ">=5.74.0"
+    }
+  }
+  backend "remote" {
+    organization = "Shanks_practice"
+
+    workspaces {
+      name = "terra_practice"
     }
   }
 }
