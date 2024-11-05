d41ae535d976df4d9e318261d065e24a1dbeb90c
shanks2690 <Shanks.git@2826>
2024-11-05 14:41:28 +0530

    My terraform practice project

---
+++ terraform code/main.tf
@@ -0,0 +1,70 @@
+
+terraform {
+  required_version = ">=1.9.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">=5.74.0"
+    }
+  }
+}
+
+
+
+provider "aws" {
+  region     = "ap-south-1"
+#  secret_key = "945KIVnq+TLGBpsfsDVO2KLPsBUv9l2r3/yZaH9C"
+#  access_key = "AKIA4SDNV5CHTD257VGC"
+}
+
+terraform {
+  backend "s3" {
+  bucket = "mybackendbucket"
+  key    = "practice/terraform.tfstate"
+  region = "ap-south-1"
+}
+}
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my-generated-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_security_group" "allow_sg" {
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.7.1"
+  tags = {
+    Name:"newEc2"
+  }
+  vpc_security_group_ids = [aws_security_group.allow_sg.id]
+  key_name = "newKeyPair"
+}
+
+#data"aws_instance" "my_ec2" {
+#  filter {
+#    name   = "tag:Name"
+#    values = ["newEc2"]
+#  }
+#
+#}
+#
+#output "ec2_details" {
+#  value = [data.aws_instance.my_ec2.ami,data.aws_instance.my_ec2.public_ip]
+#}
